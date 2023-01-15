56bd4456e5ea3dfdf82fe92c7aefa5bbbcdbaaa6
PUSHRSP <pushrsp@gmail.com>
2023-01-15 18:13:37 +0900

    #5 - object mapper
    object -> text
    text -> object
    
    json serialize & deserialize 자바 테스트

--- src/main/java/com/example/practicespring/PracticeSpringApplication.java
+++ src/main/java/com/example/practicespring/PracticeSpringApplication.java
@@ -1,13 +1,28 @@
 package com.example.practicespring;
 
-import org.springframework.boot.SpringApplication;
+import com.example.practicespring.dto.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeSpringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PracticeSpringApplication.class, args);
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // object -> text getter 필요
+        User user = new User("hi", 1);
+        String text = objectMapper.writeValueAsString(user);
+
+        System.out.println(text);
+
+        // text -> object 기본 생성자 필요
+        User result = objectMapper.readValue(text, User.class);
+
+        System.out.println(user.getName());
+
+//        SpringApplication.run(PracticeSpringApplication.class, args);
     }
 
 }
