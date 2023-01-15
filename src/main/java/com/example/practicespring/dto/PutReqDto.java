2dbe478a3f0061226b4c6f6ab2b552b42e03d0fe
PUSHRSP <pushrsp@gmail.com>
2023-01-15 17:22:22 +0900

    #1 - PUT

---
+++ src/main/java/com/example/practicespring/dto/PutReqDto.java
@@ -0,0 +1,35 @@
+package com.example.practicespring.dto;
+
+import java.util.List;
+
+public class PutReqDto {
+
+    private String name;
+    private int age;
+    private List<CarDto> carList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<CarDto> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(List<CarDto> carList) {
+        this.carList = carList;
+    }
+
+}
